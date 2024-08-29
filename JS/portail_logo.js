37abf2fb85c7ece2d8e95d9546fd2f15c2cf0811
P-A <he202408@students.ephec.be>
2024-08-29 22:21:07 +0200

    add portail

--- JS/portail_logo.js
+++ JS/portail_logo.js
@@ -1,14 +1,40 @@
 let choixUtilisateur
-let newLogoPath = document.querySelector('.img-logo').src
+let newLogoPath
+
+document.addEventListener("DOMContentLoaded", () => {
+    newLogoPath = document.querySelector('.img-logo').src
+    document.querySelector('.img-logo').onclick = changeLogo
+    chrome.storage.sync.get(['logochoisi'], function(result) {
+        choixUtilisateur = result.logochoisi;
+        if (choixUtilisateur === "old") {
+            //changeTheme('old')
+            const style = document.createElement('style');
+            style.type = 'text/css';
+            style.textContent = `
+                                    .profil[b-p2i6fi6whn]{
+                                        background-color: #F47A20 !important;
+                                    }
+                                    
+                                    div .bg-primary[b-p2i6fi6whn]{
+                                        background-color: #F47A20 !important;
+                                    }`
+            ;  // This is the CSS code defined by the user
+            document.head.appendChild(style);  // Or document.documentElement (the <html> element)
+            libChangeLogo(chrome.runtime.getURL('IMG/old-logo-ephec-portail.png'));
+        }
+    })
+})
 
 function changeLogo (){
 
     if (choixUtilisateur === "old"){
-        changeTheme('new')
+        //changeTheme('new')
+        libChangeLogo(newLogoPath);
         chrome.storage.sync.set({ 'logochoisi': 'new' })
         choixUtilisateur = "new"
     }else{
-        changeTheme('old')
+        //changeTheme('old')
+        libChangeLogo(chrome.runtime.getURL('IMG/old-logo-ephec-portail.png'));
         chrome.storage.sync.set({ 'logochoisi': 'old' })
         choixUtilisateur = "old"
     }
@@ -25,15 +51,15 @@ function libChangeLogo (IMGpath){
         changeTheme('old')
     }
 });
-*/
 
-document.querySelector('.img-logo').onclick = changeLogo
+
+
 
 function changeTheme(theme){
     libChangeLogo((theme === 'old' ? chrome.runtime.getURL('IMG/old-logo-ephec-portail.png') : newLogoPath));
     document.querySelectorAll('.profil[b-p2i6fi6whn], div .bg-primary[b-p2i6fi6whn]').forEach(el => {
         el.setAttribute('style', `background-color: ${(theme === 'old' ? '#F47A20' : '#1F2A37')} !important;`);
     })
-}
+}*/
 
 
